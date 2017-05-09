ef7403962df8f393ae766f6385db538dc044e239
sorablaze_11 <limburaj65@gmail.com>
2017-05-10 02:53:02 +0530

    Create ProjectEuler010.cpp

---
+++ ProjectEuler010.cpp
@@ -0,0 +1,45 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isPrime(unsigned long long int no){
+    if(no==2 || no==3)
+        return true;
+    if(no%2==0 || no%3==0)
+        return false;
+    for(int l=3;l*l<=no;l+=2){
+        if(no%l==0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+    unsigned long long int t, n;
+    vector<unsigned long long int> arr{2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+    unsigned long long int sum = 0;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>n;
+        sum = 0;
+        if(arr[arr.size()-1] > n){
+            for(int j=0;arr[j]<=n;j++)
+                sum += arr[j];
+        }else{
+            for(int j=0;j<arr.size();j++)
+                sum += arr[j];
+            for(unsigned long long int k=arr[arr.size()-1]+2;k<=n;k+=2){
+                if(isPrime(k)){
+                    arr.push_back(k);
+                    sum += k;
+                }
+                
+            }
+        }
+        cout<<sum<<"\n";
+    }
+    return 0;
+}
